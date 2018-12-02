895b67d15a5502c5e0d7347e8b3429761a313896
Adwoa Boakye <adwoa@movementdash.com>
2018-12-02 19:58:27 +0200

    Removing extraneous standalone component files and combining functions into screen files

--- screens/LotScreen.js
+++ screens/LotScreen.js
@@ -4,18 +4,64 @@ import {
   View,
 } from 'react-native';
 
-import LotView from '../components/LotView';
+import GlobalVariables from '../constants/GlobalVariables';
 
 export default class LotScreen extends React.Component {
   static navigationOptions = {
     header: null,
+    title: 'The Lot',
   };
 
   render() {
   	return (
-  		<LotView>
-  			<Text> Map View </Text>
-  		</LotView>
+  		<LotView />
   	);
+  }
+}
+
+class LotView extends React.Component {
+  
+  constructor(props) {
+      super(props);
+  }
+
+  _loadLotView() {
+    return fetch(GlobalVariables.BASE_ROUTE + Route.FULL_LOT , {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': 'Bearer '+ GlobalVariables.LOTWING_ACCESS_TOKEN,
+          },
+      })
+      .then((response) => response.json())
+          .then((responseJson) => {
+        console.log(responseJson);
+        
+        GlobalVariables.LOTS = responseJson['parking_lots'];
+        GlobalVariables.PARKING_SPACES = responseJson['parking_spaces'];
+        GlobalVariables.BUILDINGS = responseJson['buildings'];
+      });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Lot View</Text>
+        <Mapbox.MapView
+            styleURL={Mapbox.StyleURL.Street}
+            zoomLevel={15}
+            centerCoordinate={[11.256, 43.770]}
+            style={styles.container}>
+        </Mapbox.MapView>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center", 
+    justifyContent: "center",
   }
 }
