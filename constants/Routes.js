e5c97be622d82f099c5b11300882e96ba4206651
Adwoa Boakye <adwoa@movementdash.com>
2019-01-17 11:05:09 +0200

    Adding vehicle and tag endpoints

--- constants/Routes.js
+++ constants/Routes.js
@@ -1,5 +1,19 @@
 export default {
+	// GET
+
 	FULL_LOT: '/api/shapes',
 	PARKING_LOT: '/api/shapes/parking_lots',
 	LOGIN: '/api/auth/login',
+	VEHICLE: '/api/vehicle/',
+
+	// POST
+
+	// This endpoint expects the following parameters
+	// * vehicle_id (which vehicle is this)
+	// * shape_id (which parking space is this)
+	// * event_type (is it a "tag", "note", "test drive", etc)
+	// * event_details (for notes/test drives/etc, leave a comment here)
+	TAG_VEHICLE: 'api/tags/',
+
+	
 }
