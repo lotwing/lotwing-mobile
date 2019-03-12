678119ada67dbd688a232327eeed161750014678
Adwoa Boakye <adwoa@movementdash.com>
2019-03-12 14:06:39 +0200

    Adding note tag functionality

--- screens/NoteScreen.js
+++ screens/NoteScreen.js
@@ -6,6 +6,7 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   TouchableOpacity,
   View,
 } from 'react-native';
@@ -17,48 +18,164 @@ import buttonStyles from '../constants/ButtonStyles';
 import pageStyles from '../constants/PageStyles';
 import textStyles from '../constants/TextStyles';
 
+import LotActionHelper from '../helpers/LotActionHelper';
+
 export default class NoteScreen extends React.Component {
 	constructor(props) {
 		super(props);
 		this.details = this.props.navigation.state.params;
+		this.showSaveTagViews = this.showSaveTagViews.bind(this);
+		this.sendNoteData = this.sendNoteData.bind(this);
+
+		this.state = {
+			isNoteActionVisible: true,
+			placeholderText: 'Write your vehicle note here.'
+		}
+	}
+
+	showSaveTagViews() {
+		this.setState({isNoteActionVisible: false});
+	}
+
+	sendNoteData() {
+		console.log('\nsendFuelData called');
+		console.log('\nNote: ', this.state.placeholderText);
+
+		//TODO(adwoa): make save button unclickable, process this action
+		let space_data = LotActionHelper.structureTagPayload('note', this.details, this.state.placeholderText);
+		let noteScreen = this;
+		console.log('TAG DATA: ', space_data);
+
+		return fetch(GlobalVariables.BASE_ROUTE + Route.TAG_VEHICLE , {
+		    method: 'POST',
+		    headers: {
+		      'Accept': 'application/json',
+		      'Content-Type': 'application/json',
+		      'Authorization': 'Bearer '+ GlobalVariables.LOTWING_ACCESS_TOKEN,
+		    },
+		    body: JSON.stringify(space_data),
+		  })
+		  .then((response) => {
+		    return response.json();
+		  })
+		  .then((responseJson) => {
+		    LotActionHelper.backAction(this.props.navigation);
+		  })
+		  .catch(err => {
+		    console.log('\nCAUHT ERROR: \n', err, err.name);
+		    //TODO(adwoa): make save button clickable again
+		    return err
+		  });
+	}
+
+	_renderProperNoteActionView() {
+		if (this.state.isNoteActionVisible) {
+			return (
+				<View
+					style={{flex:7, alignItems: 'center', justifyContent: 'center'}}>
+					
+					<View style={[pageStyles.noteCard, {marginTop: '20%'}]}>
+		  				<TextInput
+							editable={true}
+							multiline={true}
+							onChangeText={(placeholderText) => this.setState({placeholderText})}
+		 					placeholder={this.state.placeholderText} />
+		  			</View>
+
+		  			<View style={
+			  				[
+			  					pageStyles.row, 
+			  					{flex:1, justifyContent: 'center', alignItems: 'center', margin: 30}
+			  				]}>
+			  			<TouchableOpacity style={
+			  				[
+			  					buttonStyles.activeSecondaryModalButton,
+			  					{width: '90%', paddingTop: 15, paddingBottom: 15}
+			  				]}
+			  				onPress={this.sendNoteData}>
+			  				<Text style={[buttonStyles.activeSecondaryTextColor, {fontWeight: '300', fontSize: 20}]}>
+			  					SAVE NOTE
+			  				</Text>
+			  			</TouchableOpacity>
+		  			</View>
+		  		</View>
+	  			);
+
+		} else {
+			// This batch of code is not currently ever reached.... we don't need a 
+			// second confirmation screen when you're trying to save a note. Either
+			// click save or navigate back
+
+			// TODO(adwoa): either delete this or make the thing below a component that's more easily callable
+
+			return (
+				<View 
+	  				style={{flex:7, alignItems: 'center', justifyContent: 'center'}}>
+	  				<View
+	  					style={pageStyles.noteCard}>
+		  				<Text
+		  					style={textStyles.actionSummaryHeader}>
+		  					Summary
+		  				</Text>
+		  				<Text
+		  					style={[textStyles.actionSummaryText, {marginTop: 15}]}>
+		  					Vehicle note: {this.state.placeholderText}
+		  				</Text>
+		  			</View>
+
+		  			<View
+		  				style={{flexDirection:'row', marginTop: 40}}>
+			  			<TouchableOpacity style={
+			  				[
+			  					buttonStyles.activeSecondaryModalButton,
+			  					{width: '40%', paddingTop: 15, paddingBottom: 15}
+			  				]}
+			  				onPress={() => {LotActionHelper.backAction(this.props.navigation)}}>
+			  				<Text style={[buttonStyles.activeSecondaryTextColor, {fontWeight: '300', fontSize: 20}]}>
+			  					CANCEL
+			  				</Text>
+			  			</TouchableOpacity>
+
+				  		<TouchableOpacity style={
+			  				[
+			  					buttonStyles.activePrimaryModalButton,
+			  					{width: '40%', paddingTop: 15, paddingBottom: 15}
+			  				]}
+			  				onPress={() => {this.sendFuelData()}}>
+			  				<Text style={[buttonStyles.activePrimaryTextColor, {fontWeight: '300', fontSize: 20}]}>
+			  					SAVE
+			  				</Text>
+			  			</TouchableOpacity>
+			  		</View>
+
+	  			</View>
+	  			);
+		}
 	}
 
 	render() {
   	return (
   		<View style={[pageStyles.container, {justifyContent: 'flex-start', backgroundColor: '#E6E4E0'}]}>
 	  		<View style={[pageStyles.darkBody, pageStyles.row, {justifyContent: 'space-between'}]}>
+	  			
 	  			<View style={[pageStyles.darkBody, pageStyles.column]}>
 	  				<Text style={textStyles.header}>
-	            {this.details.year} {this.details.make} {this.details.model}</Text>
-	          <Text style={textStyles.subtitle}>
-	            {this.details.vehicleId}</Text>
+		            	{this.details.year} {this.details.make} {this.details.model}
+		            </Text>
+		          	<Text style={textStyles.subtitle}>
+		          		{this.details.vehicleId}
+		          	</Text>
 	  			</View>
+
 	  			<View style={pageStyles.column}>
-	          <Image
-	            source={require('../assets/images/note-white.png')}
-	            style={[buttonStyles.icon, {padding: 10, minWidth: 30}]}
-	            resizeMode={"contain"}/>
-	        </View>
+		          <Image
+		            source={require('../assets/images/note-white.png')}
+		            style={[buttonStyles.icon, {padding: 10, minWidth: 30}]}
+		            resizeMode={"contain"}/>
+		        </View>
   			</View>
 
-  			<View style={{flex: 4}}>
-  			</View>
-
-  			<View style={
-	  				[
-	  					pageStyles.row, 
-	  					{flex:1, justifyContent: 'center', alignItems: 'center', margin: 30}
-	  				]}>
-	  			<TouchableOpacity style={
-	  				[
-	  					buttonStyles.activeSecondaryModalButton,
-	  					{width: '90%', paddingTop: 15, paddingBottom: 15}
-	  				]}>
-	  				<Text style={[buttonStyles.activeSecondaryTextColor, {fontWeight: '300', fontSize: 20}]}>
-	  					SAVE NOTE
-	  				</Text>
-	  			</TouchableOpacity>
-  			</View>
+  			{this._renderProperNoteActionView()}
 
   		</View>
   	);
