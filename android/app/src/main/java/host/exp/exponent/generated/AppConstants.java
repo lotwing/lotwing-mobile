c31214d9dd42ead0d5edaefbdf2cc7019b3ee685
Damien Mason <damien@gooddogdesign.com>
2019-07-13 16:33:26 +1000

    publish

--- android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -31,10 +31,11 @@ public class AppConstants {
         
         
         
+        
         // ADD EMBEDDED RESPONSES HERE
         // START EMBEDDED RESPONSES
         embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@damienmason/lotwing-mobile", "assets://shell-app-manifest.json", "application/json"));
-        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40damienmason%2Flotwing-mobile%2F1.0.0%2F9e72211f823a8079f6479fdce6edd780-31.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40damienmason%2Flotwing-mobile%2F1.0.0%2Fa88bae8edb751295987ca6b5d1a92a1a-31.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
         // END EMBEDDED RESPONSES
     EMBEDDED_RESPONSES = embeddedResponses;
   }
